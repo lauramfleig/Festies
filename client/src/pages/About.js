e0a11e1106efd8f41a0f4edf6a08a08d244945f5
Laura Fleig <laurafleig@Lauras-MacBook-Pro.local>
2018-04-07 13:58:28 -0700

    changed server.js for Passport, worked on login button on About page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Logo from '../components/Logo';
+import LoginButton from '../components/Login/LoginButton'
+import LoginWindow from '../components/Login/LoginWindow'
 
 /* import Banner from '../components/Banner';
 import TopInfo from '../components/TopInfo';
@@ -9,11 +11,35 @@ import Login from '../components/Login'; */
 
 class About extends Component {
 
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            showLogin: false
+        }
+    }
+    renderLogin = () => {
+        console.log("show modal");
+        this.setState({
+            showLogin: true
+        });
+    }
+
+// turnary
+// var eval = (condition) ? ifTrue : ifFalse
     render() {
+        const modal = this.state.showLogin ? (
+                    <LoginWindow />
+                ) : (
+                 null
+                )
+
         return (
             <div className="about-container">
             <Logo />
+            <LoginButton handleClick={() => this.renderLogin} />
+
+            { modal }
             {/* <Banner />
             <TopInfo />
             <BottomInfo />
