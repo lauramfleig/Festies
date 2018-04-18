6575bed82fe755819e452cb22e7963abc06b2839
MacbookPro <andreesito@gmail.com>
2018-04-18 00:29:10 -0700

    backend - passport auth started

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
+/* const passport = require('passport');
+require('../config/passport')(passport); */
 const userController = require('../controllers/user');
+
+
 
 // SONGKICK API ROUTES
 
@@ -17,7 +20,7 @@ router.get('/api/city/:citySearch', userController.searchCity);
 
 // ------------------- POST ROUTES
 
-router.post('/api/new_user', userController.newUser);
+router.post('/api/new_user', /* passport.authenticate('jwt', { session: false }), */ userController.newUser);
 // ------------------- PUT ROUTES
 
 
