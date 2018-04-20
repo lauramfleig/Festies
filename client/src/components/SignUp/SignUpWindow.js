7a9465dec4767dd675b5592db30833840c2efbc6
Laura Fleig <laurafleig@Lauras-MacBook-Pro.local>
2018-04-19 20:39:04 -0700

    login and register complete

--- client/src/components/SignUp/SignUpWindow.js
+++ client/src/components/SignUp/SignUpWindow.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import './LoginWindow.css';
-import Register from '../Register/Register.js'
+import './SignUpWindow.css'
 
-
-class LoginWindow extends Component {
+class SignUpWindow extends Component {
     // Setting the component's initial state
     state = {
         email: "",
@@ -24,25 +22,25 @@ class LoginWindow extends Component {
     handleFormSubmit = event => {
         // Preventing the default behavior of the form submit (which is to refresh the page)
         event.preventDefault();
-       
+
         let email = this.state.email
         let password = this.state.password
 
         console.log('Email: ' + email + 'Password: ' + password)
 
         this.setState({
-           email: "",
-           password: ""
+            email: "",
+            password: ""
         });
     };
 
     render() {
         // Notice how each input has a `value`, `name`, and `onChange` prop
         return (
-            <div className="form-div">
+            <div className="register-form-div">
                 <form className="login-form">
-                <div className="login-form-holder">
-                    <h1 className="email-text">Email</h1>
+                    <div className="login-form-holder">
+                        <h1 className="email-text">Email</h1>
                         <input
                             value={this.state.email}
                             name="email"
@@ -51,7 +49,7 @@ class LoginWindow extends Component {
                             placeholder="Email"
                         />
 
-                    <br />
+                        <br />
 
                         <h1 className="email-text">Password</h1>
                         <input
@@ -62,18 +60,12 @@ class LoginWindow extends Component {
                             placeholder="Password"
                         />
 
-                    <br />
-                    <br />
+                        <br />
+                        <br />
 
                         <div className="submit-btn-div">
                             <button className="submit-btn" onClick={this.handleFormSubmit}>Submit</button>
                         </div>
-
-                        <div className="register-btn-div">
-                            <button className="register-btn" onClick={this.props.renderSignUp}>SignUp</button>
-                        </div>
-
-
 
                     </div>
                 </form>
@@ -82,4 +74,4 @@ class LoginWindow extends Component {
     }
 }
 
-export default LoginWindow;
+export default SignUpWindow;
